e8ec309498108d07991b8e6c7698d86488ae1923
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-04-13 20:32:02 +0900

    bug fix

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,5 +3,5 @@
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-gcc ${cwd}/test.cpp -lstdc++ && ${cwd}/a.out
+gcc ${cwd}/main.cpp -lstdc++ && ${cwd}/a.out
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
-#include <gtest/gtest.h>
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-    EXPECT_EQ(2,1);
+int main(void){
+  cout<<"hello world"<<endl;
 }
+
+
 
