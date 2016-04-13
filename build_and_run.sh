c1a6576788942b5572d240d6123284ca99b2a76a
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-04-13 20:29:40 +0900

    add files

---
+++ build_and_run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# get file path
+cwd=`dirname "${0}"`
+expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
+
+gcc ${cwd}/test.cpp -lstdc++ && ${cwd}/a.out
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include <gtest/gtest.h>
+using namespace std;
+
+TEST(TestCaseName, TestName) {
+    EXPECT_EQ(2,1);
+}
+
